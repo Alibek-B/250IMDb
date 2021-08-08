e7364d1431cbc3dcae2e04e1f814cdcb33465d44
alibek <bajmuhametovalibek30@gmail.com>
2021-08-08 09:26:04 +0300

    shortened link, add gems in Gemfile

--- lib/film_collection.rb
+++ lib/film_collection.rb
@@ -6,7 +6,7 @@ class FilmCollection
 
   def self.from_wiki
     films = []
-    doc = Nokogiri::HTML(URI.open("https://ru.wikipedia.org/wiki/250_%D0%BB%D1%83%D1%87%D1%88%D0%B8%D1%85_%D1%84%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B2_%D0%BF%D0%BE_%D0%B2%D0%B5%D1%80%D1%81%D0%B8%D0%B8_IMDb"))
+    doc = Nokogiri::HTML(URI.open("https://clck.ru/9HXwE"))
 
     doc.css("tbody tr")[1..250].map do |film|
       title = film.css("td")[1].content
